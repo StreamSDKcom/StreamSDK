4829a5e7103d0a8d68181eeb45e8e5b155a52425
StreamSDKcom <github@streamsdk.com>
2020-04-23 10:08:19 -0400

    Update MicTest.cs
    MicTest required preprocessor tag for WebGL builds

--- Tool/MicTest/MicTest.cs
+++ Tool/MicTest/MicTest.cs
@@ -7,8 +7,10 @@ public class MicTest : MonoBehaviour
     // Start recording with built-in Microphone and play the recorded audio right away
     void Start()
     {
+		#if !UNITY_WEBGL
         AudioSource audioSource = GetComponent<AudioSource>();
         audioSource.clip = Microphone.Start("", true, 10, 44100);
         audioSource.Play();
+		#endif
     }
 }
